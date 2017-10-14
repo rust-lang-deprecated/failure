a1bafcdb7c716ac7dc5fb6947677d48931e4d914
Without Boats <woboats@gmail.com>
2017-10-14 13:12:34 -0700

    Split tests into two files.

---
+++ derive-fail/tests/backtrace.rs
@@ -0,0 +1,62 @@
+#![feature(attr_literals)]
+
+extern crate failure;
+#[macro_use] extern crate derive_fail;
+
+use failure::{Fail, Backtrace};
+
+#[derive(Fail, Debug)]
+#[error_msg("Error code: {}", code)]
+struct BacktraceError {
+    backtrace: Backtrace,
+    code: u32,
+}
+
+#[test]
+fn backtrace_error() {
+    let err = BacktraceError { backtrace: Backtrace::new(), code: 7 };
+    let s = format!("{}", err.display());
+    assert_eq!(&s[..], "Error code: 7");
+    assert!(err.backtrace().is_some());
+}
+
+#[derive(Fail, Debug)]
+#[error_msg("An error has occurred.")]
+struct BacktraceTupleError(Backtrace);
+
+#[test]
+fn backtrace_tuple_error() {
+    let err = BacktraceTupleError(Backtrace::new());
+    let s = format!("{}", err.display());
+    assert_eq!(&s[..], "An error has occurred.");
+    assert!(err.backtrace().is_some());
+}
+
+#[derive(Fail, Debug)]
+enum BacktraceEnumError {
+    #[error_msg("Error code: {}", code)]
+    StructVariant {
+        code: i32,
+        backtrace: Backtrace,
+    },
+    #[error_msg("Error: {}", 0)]
+    TupleVariant(&'static str, Backtrace),
+    #[error_msg("An error has occurred.")]
+    UnitVariant,
+}
+
+#[test]
+fn backtrace_enum_error() {
+    let err = BacktraceEnumError::StructVariant { code: 2, backtrace: Backtrace::new() };
+    let s = format!("{}", err.display());
+    assert_eq!(&s[..], "Error code: 2");
+    assert!(err.backtrace().is_some());
+    let err = BacktraceEnumError::TupleVariant("foobar", Backtrace::new());
+    let s = format!("{}", err.display());
+    assert_eq!(&s[..], "Error: foobar");
+    assert!(err.backtrace().is_some());
+    let err = BacktraceEnumError::UnitVariant;
+    let s = format!("{}", err.display());
+    assert_eq!(&s[..], "An error has occurred.");
+    assert!(err.backtrace().is_none());
+}
--- derive-fail/tests/tests.rs
+++ derive-fail/tests/tests.rs
@@ -1,10 +1,8 @@
 #![feature(attr_literals)]
 
-extern crate backtrace;
 extern crate failure;
 #[macro_use] extern crate derive_fail;
 
-use backtrace::Backtrace;
 use failure::Fail;
 
 #[derive(Fail, Debug)]
@@ -30,21 +28,6 @@ fn record_struct() {
 }
 
 #[derive(Fail, Debug)]
-#[error_msg("Error code: {}", code)]
-struct BacktraceError {
-    backtrace: Backtrace,
-    code: u32,
-}
-
-#[test]
-fn backtrace_error() {
-    let err = BacktraceError { backtrace: Backtrace::new(), code: 7 };
-    let s = format!("{}", err.display());
-    assert_eq!(&s[..], "Error code: 7");
-    assert!(err.backtrace().is_some());
-}
-
-#[derive(Fail, Debug)]
 #[error_msg("Error code: {}", 0)]
 struct TupleError(i32);
 
@@ -52,18 +35,6 @@ struct TupleError(i32);
 fn tuple_struct() {
     let s = format!("{}", TupleError(2).display());
     assert_eq!(&s[..], "Error code: 2");
-}
-
-#[derive(Fail, Debug)]
-#[error_msg("An error has occurred.")]
-struct BacktraceTupleError(Backtrace);
-
-#[test]
-fn backtrace_tuple_error() {
-    let err = BacktraceTupleError(Backtrace::new());
-    let s = format!("{}", err.display());
-    assert_eq!(&s[..], "An error has occurred.");
-    assert!(err.backtrace().is_some());
 }
 
 #[derive(Fail, Debug)]
@@ -86,33 +57,4 @@ fn enum_error() {
     assert_eq!(&s[..], "Error: foobar");
     let s = format!("{}", EnumError::UnitVariant.display());
     assert_eq!(&s[..], "An error has occurred.");
-}
-
-#[derive(Fail, Debug)]
-enum BacktraceEnumError {
-    #[error_msg("Error code: {}", code)]
-    StructVariant {
-        code: i32,
-        backtrace: Backtrace,
-    },
-    #[error_msg("Error: {}", 0)]
-    TupleVariant(&'static str, Backtrace),
-    #[error_msg("An error has occurred.")]
-    UnitVariant,
-}
-
-#[test]
-fn backtrace_enum_error() {
-    let err = BacktraceEnumError::StructVariant { code: 2, backtrace: Backtrace::new() };
-    let s = format!("{}", err.display());
-    assert_eq!(&s[..], "Error code: 2");
-    assert!(err.backtrace().is_some());
-    let err = BacktraceEnumError::TupleVariant("foobar", Backtrace::new());
-    let s = format!("{}", err.display());
-    assert_eq!(&s[..], "Error: foobar");
-    assert!(err.backtrace().is_some());
-    let err = BacktraceEnumError::UnitVariant;
-    let s = format!("{}", err.display());
-    assert_eq!(&s[..], "An error has occurred.");
-    assert!(err.backtrace().is_none());
 }
