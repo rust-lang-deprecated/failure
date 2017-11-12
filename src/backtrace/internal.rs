eaa02d52c02b43e83ef9c82efb6953706d07a33a
Without Boats <boats@mozilla.com>
2017-11-11 22:27:30 -0800

    Backtrace variable supports fallback.
    If the RUST_FAILURE_BACKTRACE variable is set, that overrides the
    general-purpose RUST_BACKTRACE variable.

--- src/backtrace/internal.rs
+++ src/backtrace/internal.rs
@@ -1,12 +1,14 @@
 use std::cell::UnsafeCell;
 use std::env;
+use std::ffi::OsString;
 use std::fmt;
 use std::sync::atomic::{AtomicUsize, ATOMIC_USIZE_INIT, Ordering};
 use std::sync::Mutex;
 
 pub use super::backtrace::Backtrace;
 
-const BACKTRACE_VAR: &str = "RUST_ERROR_BACKTRACE";
+const GENERAL_BACKTRACE: &str = "RUST_BACKTRACE";
+const FAILURE_BACKTRACE: &str = "RUST_FAILURE_BACKTRACE";
 
 pub(super) struct InternalBacktrace {
     backtrace: Option<MaybeResolved>,
@@ -26,10 +28,7 @@ impl InternalBacktrace {
 
         match ENABLED.load(Ordering::SeqCst) {
             0 => {
-                let enabled = match env::var_os(BACKTRACE_VAR) {
-                    Some(ref val) if val != "0" => true,
-                    _ => false,
-                };
+                let enabled = is_backtrace_enabled(|var| env::var_os(var));
                 ENABLED.store(enabled as usize + 1, Ordering::SeqCst);
                 if !enabled {
                     return InternalBacktrace { backtrace: None }
@@ -72,5 +71,56 @@ impl fmt::Debug for InternalBacktrace {
         f.debug_struct("InternalBacktrace")
             .field("backtrace", &self.as_backtrace())
             .finish()
+    }
+}
+
+fn is_backtrace_enabled<F: Fn(&str) -> Option<OsString>>(get_var: F) -> bool {
+    match get_var(FAILURE_BACKTRACE) {
+        Some(ref val) if val != "0" => true,
+        Some(ref val) if val == "0" => false,
+        _ => match get_var(GENERAL_BACKTRACE) {
+            Some(ref val) if val != "0" => true,
+            _                           => false,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const YEA: Option<&str> = Some("1");
+    const NAY: Option<&str> = Some("0");
+    const NOT_SET: Option<&str> = None;
+
+    macro_rules! test_enabled {
+        (failure: $failure:ident, general: $general:ident => $result:expr) => {{
+            assert_eq!(is_backtrace_enabled(|var| match var {
+                FAILURE_BACKTRACE   => $failure.map(OsString::from),
+                GENERAL_BACKTRACE   => $general.map(OsString::from),
+                _                   => panic!()
+            }), $result);
+        }}
+    }
+
+    #[test]
+    fn always_enabled_if_failure_is_set_to_yes() {
+        test_enabled!(failure: YEA, general: YEA => true);
+        test_enabled!(failure: YEA, general: NOT_SET => true);
+        test_enabled!(failure: YEA, general: NAY => true);
+    }
+
+    #[test]
+    fn never_enabled_if_failure_is_set_to_no() {
+        test_enabled!(failure: NAY, general: YEA => false);
+        test_enabled!(failure: NAY, general: NOT_SET => false);
+        test_enabled!(failure: NAY, general: NAY => false);
+    }
+
+    #[test]
+    fn follows_general_if_failure_is_not_set() {
+        test_enabled!(failure: NOT_SET, general: YEA => true);
+        test_enabled!(failure: NOT_SET, general: NOT_SET => false);
+        test_enabled!(failure: NOT_SET, general: NAY => false);
     }
 }
