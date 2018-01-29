b3346668674e048058026451e29a4e5a99888c4b
boats <boats@mozilla.com>
2018-01-29 14:20:51 -0800

    Fix travis.

--- travis.sh
+++ travis.sh
@@ -3,13 +3,15 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 run_tests_in() {
-   cd $1
-   cargo test
-   cargo test --no-default-features
-   cd $DIR
+  cd $1
+  "echo run tests in $1"
+  cargo test
+  cargo test --no-default-features
+  cd $DIR
 }
 
 main() {
+  echo "begin main"
   run_tests_in "$DIR/failure-1.X"
   run_tests_in "$DIR/failure-0.1.X"
 }
