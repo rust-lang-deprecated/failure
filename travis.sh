eedf5ebe72d49bcf2e3394f02aafab634750af6b
boats <boats@mozilla.com>
2018-01-30 13:10:43 -0800

    Test this feature

--- travis.sh
+++ travis.sh
@@ -4,16 +4,24 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 run_tests_in() {
   cd $1
-  "echo run tests in $1"
   cargo test
   cargo test --no-default-features
+  cargo test --features backtrace
+  cd $DIR
+}
+
+test_nightly_features_in() {
+  cd $1
+  cargo test --features small-error
   cd $DIR
 }
 
 main() {
-  echo "begin main"
   run_tests_in "$DIR/failure-1.X"
   run_tests_in "$DIR/failure-0.1.X"
+  if [ "${TRAVIS_RUST_VERSION}" = "nightly" ]; then
+    test_nightly_features_in "$DIR/failure-1.X"
+  fi
 }
 
 main
