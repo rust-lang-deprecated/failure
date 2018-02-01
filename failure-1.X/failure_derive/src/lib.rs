e1e87b089d4321a2e3b7e362340f9cb8c3a291e3
boats <boats@mozilla.com>
2018-02-01 15:04:52 -0800

    Use separate derive for Display.

--- failure-1.X/failure_derive/src/lib.rs
+++ failure-1.X/failure_derive/src/lib.rs
@@ -55,94 +55,9 @@ fn fail_derive(s: synstructure::Structure) -> quote::Tokens {
         }
     });
 
-    #[cfg(feature = "std")]
-    let display = display_body(&s).map(|display_body| {
-        s.bound_impl("::std::fmt::Display", quote! {
-            #[allow(unreachable_code)]
-            fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
-                match *self { #display_body }
-                write!(f, "An error has occurred.")
-            }
-        })
-    });
-
-    #[cfg(not(feature = "std"))]
-    let display = display_body(&s).map(|display_body| {
-        s.bound_impl("::core::fmt::Display", quote! {
-            #[allow(unreachable_code)]
-            fn fmt(&self, f: &mut ::core::fmt::Formatter) -> ::core::fmt::Result {
-                match *self { #display_body }
-                write!(f, "An error has occurred.")
-            }
-        })
-    });
-
     quote! {
         #fail
-        #display
     }
-}
-
-fn display_body(s: &synstructure::Structure) -> Option<quote::Tokens> {
-    let mut msgs = s.variants().iter().map(|v| find_error_msg(&v.ast().attrs));
-    if msgs.all(|msg| msg.is_none()) { return None; }
-
-    Some(s.each_variant(|v| {
-        let msg = find_error_msg(&v.ast().attrs).expect("All variants must have display attribute.");
-        if msg.is_empty() {
-            panic!("Expected at least one argument to fail attribute");
-        }
-
-        let s = match msg[0] {
-            syn::NestedMetaItem::MetaItem(syn::MetaItem::NameValue(ref i, ref lit)) if i == "display" => {
-                lit.clone()
-            }
-            _ => panic!("Fail attribute must begin `display = \"\"` to control the Display message."),
-        };
-        let args = msg[1..].iter().map(|arg| match *arg {
-            syn::NestedMetaItem::Literal(syn::Lit::Int(i, _)) => {
-                let bi = &v.bindings()[i as usize];
-                quote!(#bi)
-            }
-            syn::NestedMetaItem::MetaItem(syn::MetaItem::Word(ref id)) => {
-                if id.as_ref().starts_with("_") {
-                    if let Ok(idx) = id.as_ref()[1..].parse::<usize>() {
-                        let bi = &v.bindings()[idx];
-                        return quote!(#bi)
-                    }
-                }
-                for bi in v.bindings() {
-                    if bi.ast().ident.as_ref() == Some(id) {
-                        return quote!(#bi);
-                    }
-                }
-                panic!("Couldn't find a field with this name!");
-            }
-            _ => panic!("Invalid argument to fail attribute!"),
-        });
-
-        quote! {
-            return write!(f, #s #(, #args)*)
-        }
-    }))
-}
-
-fn find_error_msg(attrs: &[syn::Attribute]) -> Option<&[syn::NestedMetaItem]> {
-    let mut error_msg = None;
-    for attr in attrs {
-        if attr.name() == "fail" {
-            if error_msg.is_some() {
-                panic!("Cannot have two display attributes")
-            } else {
-                if let syn::MetaItem::List(_, ref list)  = attr.value {
-                    error_msg = Some(&list[..]);
-                } else {
-                    panic!("fail attribute must take a list in parantheses")
-                }
-            }
-        }
-    }
-    error_msg
 }
 
 fn is_backtrace(bi: &&synstructure::BindingInfo) -> bool {
