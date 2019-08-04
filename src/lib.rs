c4036d8be6bcadf9fcbf0a4135506e2ee5000ff6
Tom Parker-Shemilt <palfrey@tevp.net>
2019-08-04 15:47:40 +0100

    Add allow for bare_trait_objects

--- src/lib.rs
+++ src/lib.rs
@@ -23,6 +23,7 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 #![deny(missing_docs)]
 #![deny(warnings)]
+#![allow(bare_trait_objects)] // Allowed so we have compatibility with Rust < 1.27
 #![cfg_attr(feature = "small-error", feature(extern_types, allocator_api))]
 
 macro_rules! with_std { ($($i:item)*) => ($(#[cfg(feature = "std")]$i)*) }
